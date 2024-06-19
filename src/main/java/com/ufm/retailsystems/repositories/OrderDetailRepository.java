30b6c7b7e79b2fb33b777812201176b774a0edda
huytran2510 <huy251003@gmail.com>
2024-06-19 22:25:53 +0700

    update project

---
+++ src/main/java/com/ufm/retailsystems/repositories/OrderDetailRepository.java
@@ -0,0 +1,2 @@
+package com.ufm.retailsystems.repositories;public interface OrderDetailRepository {
+}
---
+++ src/main/java/com/ufm/retailsystems/services/OrderDetailServiceImpl.java
@@ -0,0 +1,2 @@
+package com.ufm.retailsystems.services;public class OrderDetailServiceImpl {
+}
